73a21f89a4f204d65335c41a8d5b867676336fd2
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-09-29 16:45:19 +0300

    adding proto functions

--- src/server.rs
+++ src/server.rs
@@ -3,7 +3,11 @@ use tonic::{transport::Server, Code, Request, Response, Status};
 pub mod zkp_auth {
     include!("./zkp_auth.rs");
 }
-use zkp_auth::auth_server;
+use zkp_auth::{
+    auth_server::{Auth, AuthServer},
+    AuthenticationAnswerRequest, AuthenticationAnswerResponse, AuthenticationChallengeRequest,
+    AuthenticationChallengeResponse, RegisterRequest, RegisterResponse,
+};
 
 fn main() {
     println!("Hey this is server. ");
@@ -13,6 +17,31 @@ fn main() {
 // now we need to implement the traits specified in the protobuf file
 #[derive(Debug, Default)]
 struct AuthImpl {}
+
+// tonic async trait for async
+#[tonic::async_trait]
+impl Auth for AuthImpl {
+    async fn register(
+        &self,
+        request: Request<RegisterRequest>,
+    ) -> Result<Response<RegisterResponse>, Status> {
+        todo!()
+    }
+
+    async fn create_authentication_challenge(
+        &self,
+        request: Request<AuthenticationChallengeRequest>,
+    ) -> Result<Response<AuthenticationChallengeResponse>, Status> {
+        todo!()
+    }
+
+    async fn verify_authentication(
+        &self,
+        request: Request<AuthenticationAnswerRequest>,
+    ) -> Result<Response<AuthenticationAnswerResponse>, Status> {
+        todo!()
+    }
+}
 
 #[tokio::main]
 async fn main() {
