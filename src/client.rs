02b769fd6851653a9462e0bba852a4b930fc6e1e
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-10-02 09:50:29 +0300

    client register added

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hey this is client.");
+pub mod zk_auth {
+    include!("./zkp_auth.rs");
+}
+// get the user name to add maps in the server
+use std::io::{stdin, Read};
+// coming from the generated rs file using proto
+use num_bigint::BigUint;
+use zk_auth::{auth_client::AuthClient, RegisterRequest};
+use ChaumPedersen::ZKP;
+// async main
+#[tokio::main]
+async fn main() {
+    // connet to the server
+    let mut client = AuthClient::connect("http://127.0.0.1:50051")
+        .await
+        .expect("Connection failed");
+    println!("✅ Running the client");
+    println!("Username: ");
+    let mut username = String::new();
+    stdin()
+        .read_line(&mut username)
+        .expect("user name is not specified");
+
+    let mut secret = String::new();
+    stdin()
+        .read_line(&mut secret)
+        .expect("user name is not specified");
+
+    // get constants
+    let (a, b, p, q) = ZKP::get_constants();
+    // generate y1, y2
+    let request = RegisterRequest {
+        user: username.trim().to_string(),
+        y1: ZKP::mod_exp(&a, &BigUint::from_bytes_be(secret.trim().as_bytes()), &p).to_bytes_be(),
+        y2: ZKP::mod_exp(&b, &BigUint::from_bytes_be(secret.trim().as_bytes()), &p).to_bytes_be(),
+    };
+    let response = client.register(request).await.expect("Register failed");
+    println!("Response from server: {:?}", response);
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use num_bigint::BigUint;
+use std::{collections::HashMap, sync::Mutex};
 use tonic::{transport::Server, Code, Request, Response, Status};
+use ChaumPedersen::ZKP;
 
 pub mod zkp_auth {
     include!("./zkp_auth.rs");
@@ -28,7 +30,10 @@ pub struct User {
 // tokio struct is defined
 // now we need to implement the traits specified in the protobuf file
 #[derive(Debug, Default)]
-struct AuthImpl {}
+pub struct AuthImpl {
+    pub user_info: Mutex<HashMap<String, User>>,
+    pub auth_id_user: Mutex<HashMap<String, String>>,
+}
 
 // tonic async trait for async
 #[tonic::async_trait]
@@ -37,6 +42,7 @@ impl Auth for AuthImpl {
         &self,
         request: Request<RegisterRequest>,
     ) -> Result<Response<RegisterResponse>, Status> {
+        println!("Processing  Register, {:?}", request);
         // we need to generate y1 and y2 and user info (likely id)
         let request = request.into_inner();
 
@@ -44,25 +50,98 @@ impl Auth for AuthImpl {
         let y2 = BigUint::from_bytes_be(&request.y2);
 
         let mut user = User::default();
-        user.name = request.user;
+        let user_name = request.user;
+        user.name = user_name.clone();
         user.y1 = y1;
         user.y2 = y2;
+
+        let mut user_map = &mut self.user_info.lock().unwrap();
+        user_map.insert(user_name, user);
 
         Ok((Response::new(RegisterResponse {})))
     }
 
+    ///
     async fn create_authentication_challenge(
         &self,
         request: Request<AuthenticationChallengeRequest>,
     ) -> Result<Response<AuthenticationChallengeResponse>, Status> {
-        todo!()
+        println!("Challenge is being generated, {:?}", request);
+        // we need to generate r1 and r2
+        let request = request.into_inner();
+
+        let mut user_map = &mut self.user_info.lock().unwrap();
+
+        let user_name = request.user;
+
+        if let Some(user_info) = user_map.get_mut(&user_name) {
+            user_info.r1 = BigUint::from_bytes_be(&request.r1);
+            user_info.r2 = BigUint::from_bytes_be(&request.r2);
+
+            let (_, q, _, _) = ZKP::get_constants();
+            // got the order, lets call max rand for challenge
+            // let c = BigUint::from(123u32);
+            let c = ZKP::gen_rand(&q);
+            user_info.c = c.clone();
+            let auth_id = ZKP::gen_rand_string(12);
+
+            let mut auth_id_user = &mut self.auth_id_user.lock().unwrap();
+
+            auth_id_user.insert(auth_id.clone(), user_name);
+            Ok(Response::new(AuthenticationChallengeResponse {
+                auth_id: auth_id,
+                c: c.to_bytes_be(),
+            }))
+        } else {
+            Err(Status::new(
+                Code::NotFound,
+                format!("User {} not found in db", user_name),
+            ))
+        }
     }
 
     async fn verify_authentication(
         &self,
         request: Request<AuthenticationAnswerRequest>,
     ) -> Result<Response<AuthenticationAnswerResponse>, Status> {
-        todo!()
+        println!("Verification request, {:?}", request);
+        // we need to generate r1 and r2
+        let request = request.into_inner();
+
+        let mut user_map = &mut self.auth_id_user.lock().unwrap();
+        let mut user_sign_map = &mut self.user_info.lock().unwrap();
+
+        let auth_id = request.auth_id;
+
+        let instance = user_sign_map.get(&auth_id).unwrap();
+
+        if let Some(auth_id) = user_map.get_mut(&auth_id) {
+            let (a, b, p, q) = ZKP::get_constants();
+            let zkp = ZKP::init(&a, &b, &p, &q);
+            let verif = zkp.verify(
+                &instance.y1,
+                &instance.y2,
+                &instance.r1,
+                &instance.r2,
+                &instance.c,
+                &instance.s,
+            );
+            if (verif) {
+                Ok(Response::new(AuthenticationAnswerResponse {
+                    session_id: ZKP::gen_rand_string(12),
+                }))
+            } else {
+                Err(Status::new(
+                    Code::PermissionDenied,
+                    format!("Verification failed"),
+                ))
+            }
+        } else {
+            Err(Status::new(
+                Code::NotFound,
+                format!("AuthId: {} not found in database", auth_id),
+            ))
+        }
     }
 }
 
